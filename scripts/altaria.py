9363d69b44e3cd3d243c9016410137c6f2367d87
Jake Lee <jlee211@ucsc.edu>
2025-09-06 18:20:23 -0700

    Adds scripts dir.

---
+++ scripts/altaria.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+"""
+Post-processing for newly-downloaded playlists.
+"""
+include os
+
+
+def altaria():
+    """Post-processing steps for music downloaded from tidal-dl."""
+    pass
+
+if __name__ == "__main__":
+    altaria()
